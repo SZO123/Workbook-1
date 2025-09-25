d9d130e361dbf7d3677c08e08d1096c4ea740182
Najib <najib200983@outlook.com>
2025-09-25 10:02:03 -0700

    .

---
+++ src/BasicCalculator.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Enter the 1st number: ");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.print("Enter the 2nd number: ");
+        double secondNumber = scanner.nextDouble();
+
+        System.out.println("Which operation do you want to use: add, subtract, multiply, or divide?");
+        System.out.print("Please select an option: ");
+        String operation = scanner.next();
+
+        double total = firstNumber * secondNumber;
+
+        System.out.println("Answer: " + total);
+
+
+
+    }
+}
