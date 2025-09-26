59e9506b5e5e819a7606b4f3a76e8245c0ec75ad
Najib <najib200983@outlook.com>
2025-09-26 11:11:41 -0700

    .

--- src/SandwichShop.java
+++ src/SandwichShop.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class SandwichShop {
-    public static void main(String[] args, Object price) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What size sandwich would you like?");
@@ -14,18 +14,25 @@ public class SandwichShop {
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
+        double price = 0;
+        if (size == 1) {
+            price = 5.45;
+        } else if (size == 2) {
+            price = 8.95;
+        }
+
+
         if (age <= 17) {
             price = price * 0.9;
-
-            double price = 0;
-            if (size == 1) {
-                price = 5.45;
-            } else if (size == 2) {
-                price = 8.95;
-            }
-
+        } else if (age >= 65) {
+            price = price * 0.8;
 
         }
+
+        System.out.printf("Total cost of sandwich: $%.2f\n", price);
+
+    }
     }
 
-}
+
+
